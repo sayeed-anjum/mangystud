5be2453b079303a7c4d72f15d912988097e5d5eb
Sayeed Anjum <sayeed.anjum@gmail.com>
2010-12-10 09:21:48 +0530

    first commit

---
+++ grails-app/domain/org/mangystud/Tiddler.groovy
@@ -0,0 +1,24 @@
+package org.mangystud
+
+
+class Tiddler {
+
+    static constraints = {
+    }
+	
+	int id;
+	String title
+	String type
+	String context
+	boolean done
+	boolean star
+	String state
+	String area
+	String contact
+	String project
+	String notes
+	String realm
+	String dependsOn
+	Date updatedOn
+	Date createdOn
+}
---
+++ test/unit/mangystud/TiddlerTests.groovy
@@ -0,0 +1,17 @@
+package mangystud
+
+import grails.test.*
+
+class TiddlerTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
