1e1f86bc1263d625947fa2c7f144b7fde536effe
Sayeed Anjum <sayeed.anjum@gmail.com>
2010-12-29 15:48:28 +0530

    remove references to project if project is being deleted

--- grails-app/domain/org/mangystud/Project.groovy
+++ grails-app/domain/org/mangystud/Project.groovy
@@ -1,4 +1,7 @@
 package org.mangystud
 
 class Project extends Tiddler {
+	def beforeDelete = {
+		def result = Tickler.executeUpdate("update Tickler t set t.project = null where t.project = ?", [this]);
+	 }
 }
