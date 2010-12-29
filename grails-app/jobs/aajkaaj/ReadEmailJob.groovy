046ea616208fce7ba1a2df72b9e3a7ee2bd845fb
Sayeed Anjum <sayeed.anjum@gmail.com>
2010-12-29 14:32:44 +0530

    setup a cron expression

--- grails-app/jobs/aajkaaj/ReadEmailJob.groovy
+++ grails-app/jobs/aajkaaj/ReadEmailJob.groovy
@@ -2,11 +2,13 @@ package aajkaaj
 
 
 class ReadEmailJob {
-    def timeout = 12000l // execute job once in 5 seconds
 	def inboxService
 
+	static triggers = {
+		cron name: 'readEmailTrigger', cronExpression: "0 0/2 * * * ?"
+	}
+ 
 	def execute() {
-		// println 'read email job ' + new Date()
 		inboxService.readVerifyEmails()
 		inboxService.readEmails()
     }
