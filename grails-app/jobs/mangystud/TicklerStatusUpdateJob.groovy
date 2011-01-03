e8a103b9eba1863aacaf4f371becab818b7509ca
Sayeed Anjum <sayeed.anjum@gmail.com>
2011-01-03 08:22:25 +0530

    change cron expression for update tickler status from every 5 minutes to every 15 minutes

--- grails-app/jobs/mangystud/TicklerStatusUpdateJob.groovy
+++ grails-app/jobs/mangystud/TicklerStatusUpdateJob.groovy
@@ -5,7 +5,7 @@ class TicklerStatusUpdateJob {
 	def ticklerService
 	 
 	static triggers = {
-       cron name: 'cronTrigger', cronExpression: "0 0/5 * * * ?"
+       cron name: 'cronTrigger', cronExpression: "0 0/15 * * * ?"
    }
 
     def execute() {
