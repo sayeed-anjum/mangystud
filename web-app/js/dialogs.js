03f50a10053d6446e38011767ddb21e937f6bea2
Sayeed Anjum <sayeed.anjum@gmail.com>
2011-01-12 09:40:52 +0530

    added validation check to dialogs to prevent duplicates

--- web-app/js/dialogs.js
+++ web-app/js/dialogs.js
@@ -5,68 +5,99 @@ $.Class.extend("Dialog", {}, {
 		this.el = $('#' + this.name);
 		$.data(this.el[0], 'parent', this);
 		this.el.dialog({
-		    autoOpen: false,
-			buttons: {
+			autoOpen : false,
+			buttons : {
 				"OK" : this.onSubmit,
 				"Cancel" : this.onCancel
 			},
-			resizable: false,
-			modal: true
+			resizable : false,
+			modal : true
+		});
+		this.el.live('keypress', function(e) {
+		    if (e.which == 13) {
+		    	$(this).parent().find('button:first').trigger('click');
+		        e.preventDefault();
+		    }
 		});
 		return this;
 	},
 
-	onSubmit: function() {
-		var me = $.data(this, 'parent');
+	onSubmit : function(e) {
+    	var me = $.data(this, 'parent');
 		if (me && me.isValid()) {
 			$('form', this).submit();
+			return true;
 		}
+		return false;
 	},
-	
+
 	isValid : function() { return true },
 	
 	onCancel : function() {
-		$(this).dialog("close");		
+		$(this).dialog("close");
 	},
-	
+
 	close : function() {
-		$(this.el).dialog("close");		
+		$(this.el).dialog("close");
 	},
-	
+
 	show : function(event) {
 		this.beforeShow(event);
 		this.el.dialog("open");
+	},
+
+	beforeShow : function() {
 	}, 
 	
-	beforeShow : function() {}
+	find : function(s, items, key) {
+		var found = false;
+		s = s.toLowerCase();
+		$.each(items, function(index, item) {
+			if (item[key].toLowerCase() == s) {
+				found = true;
+				return false;
+			}
+		});
+		return found;
+	}, 
+	
+	getCurrentRealmId : function() {
+		var tiddler = this.manager.currentTiddler;
+		return $('.realm_select select', tiddler).val();
+	}
 });
 
 Dialog.extend("RealmDialog", {}, {
 	init : function(manager) {
 		this.title = 'New Realm';
 		this._super('realm_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
-		var name = $('[name="name"]', this.el).val();
-		if (name.trim() === "") {
+		var name = $('[name="name"]', this.el).val().trim();
+		if (name === "") {
 			alert('Please enter a name');
 			return false;
-	  }
-	  return true;
+		}
+		var realms = this.manager.getRealms();
+		if (this.find(name, realms, 'name')) {
+			alert('A realm \'' + name + '\' already exists');
+			return false;
+		}
+		return true;
 	},
 
 	beforeShow : function(event) {
 		$('[name="name"]', this.el).val('');
 		this.el.dialog("option", "title", 'New Realm');
-	}, 
-	
+	},
+
 	onSuccess : function(data, textStatus) {
 		this.close();
 
 		var span = $('<span/>', {
-			"class": "realm-tab realm-active ui-corner-tl ui-corner-tr",
-			"text": data.realm.name
+			"class" : "realm-tab realm-active ui-corner-tl ui-corner-tr",
+			"text" : data.realm.name
 		});
 		$('.realm-add').before(span);
 	}
@@ -76,15 +107,20 @@ Dialog.extend("ContextDialog", {}, {
 	init : function(manager) {
 		this.title = 'New Context';
 		this._super('context_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
 		var name = $('[name=name]', this.el).val();
 		if (name.trim() === "") {
 			alert('Please enter a name');
 			return false;
-	  }
-	  return true;
+		}
+		var contexts = this.manager.getContexts(this.getCurrentRealmId());
+		if (this.find(name, contexts, 'name')) {
+			alert('A context \'' + name + '\' already exists');
+			return false;
+		}
+		return true;
 	},
 
 	beforeShow : function(event) {
@@ -92,7 +128,7 @@ Dialog.extend("ContextDialog", {}, {
 		if (actionId) {
 			var tiddler = manager.currentTiddler;
 			var realm = $('.realm_select select', tiddler).val();
-	
+
 			$('[name=realm]', this.el).val(realm);
 			$('[name=actionId]', this.el).val(actionId);
 			$('[name=name]', this.el).val('');
@@ -100,11 +136,14 @@ Dialog.extend("ContextDialog", {}, {
 		} else {
 			alert('Unable to get action id!');
 		}
-	}, 
-	
+	},
+
 	onSuccess : function(data, textStatus) {
 		this.close();
-		var event = {event: 'newContext', data: data};
+		var event = {
+			event : 'newContext',
+			data : data
+		};
 		manager.raiseEvent('newContext', event);
 	}
 });
@@ -112,32 +151,32 @@ Dialog.extend("ContextDialog", {}, {
 Dialog.extend("ActionDialog", {}, {
 	init : function(manager) {
 		this._super('action_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
 		var name = $('[name=title]', this.el).val();
 		if (name.trim() === "") {
 			alert('Please enter a title');
 			return false;
-	  }
-	  return true;
+		}
+		return true;
 	},
 
 	beforeShow : function(event) {
 		var obj = event.currentTarget;
-		
+
 		var form = $('form', this.el);
-		
+
 		$('[name=type]', this.el).val(obj.id);
 		$('[name=title]', this.el).val('');
 		this.updateContext(obj);
 		this.el.dialog("option", "title", 'New ' + event.data.type);
-	}, 
-	
+	},
+
 	updateContext : function(obj) {
 		var p = $(obj).parent();
 		var tiddler = $(obj).closest('.tiddler');
-		
+
 		var projectId = "";
 		if ($('.project', tiddler).length) {
 			projectId = $(tiddler).attr('id').substr(10);
@@ -145,47 +184,58 @@ Dialog.extend("ActionDialog", {}, {
 		$('[name=project]', this.el).val(projectId);
 
 		var state = "";
-		if ($(p).hasClass('dc_state_Next')) state = 'Next';
-		if ($(p).hasClass('dc_state_Future')) state = 'Future';
-		if ($(p).hasClass('dc_state_WaitingFor')) state = 'WaitingFor';
+		if ($(p).hasClass('dc_state_Next'))
+			state = 'Next';
+		if ($(p).hasClass('dc_state_Future'))
+			state = 'Future';
+		if ($(p).hasClass('dc_state_WaitingFor'))
+			state = 'WaitingFor';
 		$('[name=state]', this.el).val(state);
-		
+
 		var ctx = "";
 		if ($(p).hasClass('dc_ctx')) {
 			ctx = $(p).text();
-			ctx = ctx.substr(0, ctx.length-1)
+			ctx = ctx.substr(0, ctx.length - 1)
 		}
 		$('[name=context]', this.el).val(ctx);
 
 		var contact = "";
 		if ($(p).hasClass('dc_contact')) {
 			contact = $(p).text();
-			contact = contact.substr(0, contact.length-1)
+			contact = contact.substr(0, contact.length - 1)
 		}
 		$('[name=contact]', this.el).val(contact);
 	},
-	
+
 	onSuccess : function(data, textStatus) {
 		this.close();
 		tl_viewAction(data.action.id);
-		manager.raiseEvent('actionUpdate', {event: 'newAction', data: data, id: data.action.id});
+		manager.raiseEvent('actionUpdate', {
+			event : 'newAction',
+			data : data,
+			id : data.action.id
+		});
 	}
 });
-
 
 Dialog.extend("AreaDialog", {}, {
 	init : function(manager) {
 		this.title = 'New Area';
 		this._super('area_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
 		var name = $('[name=name]', this.el).val();
 		if (name.trim() === "") {
 			alert('Please enter a name');
 			return false;
-	  }
-	  return true;
+		}
+		var areas = this.manager.getAreas(this.getCurrentRealmId());
+		if (this.find(name, areas, 'name')) {
+			alert('An area \'' + name + '\' already exists');
+			return false;
+		}
+		return true;
 	},
 
 	beforeShow : function(event) {
@@ -195,11 +245,14 @@ Dialog.extend("AreaDialog", {}, {
 		$('[name=realm]', this.el).val(realm);
 		$('[name=name]', this.el).val('');
 		this.el.dialog("option", "title", this.title);
-	}, 
+	},
 
 	onSuccess : function(data, textStatus) {
 		this.close();
-		var event = {event: 'newArea', data: data};
+		var event = {
+			event : 'newArea',
+			data : data
+		};
 		manager.raiseEvent('newArea', event);
 	}
 });
@@ -208,15 +261,20 @@ Dialog.extend("ContactDialog", {}, {
 	init : function(manager) {
 		this.title = 'New Contact';
 		this._super('contact_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
 		var name = $('[name=name]', this.el).val();
 		if (name.trim() === "") {
 			alert('Please enter a name');
 			return false;
-	  }
-	  return true;
+		}
+		var contacts = this.manager.getContacts(this.getCurrentRealmId());
+		if (this.find(name, contacts, 'name')) {
+			alert('An contact \'' + name + '\' already exists');
+			return false;
+		}
+		return true;
 	},
 
 	beforeShow : function(event) {
@@ -227,11 +285,14 @@ Dialog.extend("ContactDialog", {}, {
 		$('[name=name]', this.el).val('');
 		$('[name=email]', this.el).val('');
 		this.el.dialog("option", "title", this.title);
-	}, 
-	
+	},
+
 	onSuccess : function(data, textStatus) {
 		this.close();
-		var event = {event: 'newContact', data: data};
+		var event = {
+			event : 'newContact',
+			data : data
+		};
 		manager.raiseEvent('newContact', event);
 	}
 });
@@ -240,20 +301,20 @@ Dialog.extend("TicklerDialog", {}, {
 	init : function(manager) {
 		this.title = 'New Tickler';
 		this._super('tickler_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
-	   var name = $('[name=title]', this.el).val();
-	   if (name.trim() === "") {
+		var name = $('[name=title]', this.el).val();
+		if (name.trim() === "") {
 			alert('Please enter a title');
 			return false;
-	   }
-	   var date = $('[name=date]', this.el).val();
-	   if (date.trim() === "") {
+		}
+		var date = $('[name=date]', this.el).val();
+		if (date.trim() === "") {
 			alert('Please enter a date');
 			return false;
-	   }
-	  return true;
+		}
+		return true;
 	},
 
 	beforeShow : function(event) {
@@ -261,11 +322,15 @@ Dialog.extend("TicklerDialog", {}, {
 		$('[name=title]', this.el).val('');
 		$('[name=date]', this.el).val('');
 		this.el.dialog("option", "title", this.title);
-	}, 
-	
+	},
+
 	onSuccess : function(data, textStatus) {
 		this.close();
-		manager.raiseEvent('ticklerUpdate', {event: 'newTickler', data: data, id: data.tickler.id});
+		manager.raiseEvent('ticklerUpdate', {
+			event : 'newTickler',
+			data : data,
+			id : data.tickler.id
+		});
 	}
 });
 
@@ -273,33 +338,39 @@ Dialog.extend("ProjectDialog", {}, {
 	init : function(manager) {
 		this.title = 'New Project';
 		this._super('project_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
-	   var name = $('[name=title]', this.el).val();
-	   if (name.trim() === "") {
+		var name = $('[name=title]', this.el).val();
+		if (name.trim() === "") {
 			alert('Please enter a title');
 			return false;
-	   }
-	  return true;
+		}
+		return true;
 	},
 
 	updateContext : function(obj) {
 		var p = $(obj).parent();
-		if ($(p).hasClass('dc_status_Active')) $('[name=status]', this.el).val('Active')
-		if ($(p).hasClass('dc_status_Someday')) $('[name=status]', this.el).val('Someday')
+		if ($(p).hasClass('dc_status_Active'))
+			$('[name=status]', this.el).val('Active')
+		if ($(p).hasClass('dc_status_Someday'))
+			$('[name=status]', this.el).val('Someday')
 	},
-	
+
 	beforeShow : function(event) {
 		var obj = event.currentTarget;
 		this.updateContext(obj);
 		$('[name=title]', this.el).val('');
 		this.el.dialog("option", "title", this.title);
-	}, 
-	
+	},
+
 	onSuccess : function(data, textStatus) {
 		this.close();
-		manager.raiseEvent('projectUpdate', {event: 'newProject', data: data, id: data.project.id});
+		manager.raiseEvent('projectUpdate', {
+			event : 'newProject',
+			data : data,
+			id : data.project.id
+		});
 	}
 });
 
@@ -307,29 +378,33 @@ Dialog.extend("ReferenceDialog", {}, {
 	init : function(manager) {
 		this.title = 'New Reference';
 		this._super('reference_dialog', manager);
- 	},
- 	
+	},
+
 	isValid : function() {
-	   var name = $('[name=title]', this.el).val();
-	   if (name.trim() === "") {
+		var name = $('[name=title]', this.el).val();
+		if (name.trim() === "") {
 			alert('Please enter a title');
 			return false;
-	   }
-	  return true;
+		}
+		return true;
 	},
 
 	updateContext : function(obj) {
 	},
-	
+
 	beforeShow : function(event) {
 		var obj = event.currentTarget;
 		this.updateContext(obj);
 		$('[name=title]', this.el).val('');
 		this.el.dialog("option", "title", this.title);
-	}, 
-	
+	},
+
 	onSuccess : function(data, textStatus) {
 		this.close();
-		manager.raiseEvent('referenceUpdate', {event: 'newReference', data: data, id: data.reference.id});
+		manager.raiseEvent('referenceUpdate', {
+			event : 'newReference',
+			data : data,
+			id : data.reference.id
+		});
 	}
 });
